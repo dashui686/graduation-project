9be907071d5420e7aa8a6c4ca56badfe9f9d3212
dashui686 <1327674904@qq.om>
2021-10-25 15:59:27 +0800

    整合Mybatis-plus

--- deploy/src/main/java/com/dream/deploy/controller/ProcessDeployController.java
+++ deploy/src/main/java/com/dream/deploy/controller/ProcessDeployController.java
@@ -1,12 +1,10 @@
 package com.dream.deploy.controller;
 
-import com.dream.deploy.domain.ProcessDeploy;
-import com.dream.deploy.service.ProcessDeployService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProcessDeployController {
-    @Autowired
-    ProcessDeployService
+
 }
